6d317ea4f12254844368d7c867b8a01f184553f7
shoeibi98 <46816381+shoeibi98@users.noreply.github.com>
2020-07-06 09:10:07 -0700

    add four fields in proc.h

--- proc.h
+++ proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int stime,etime,iotime,rtime;  // SYScall waitx
 };
 int getprocess(void);
 // Process memory is laid out contiguously, low addresses first:
