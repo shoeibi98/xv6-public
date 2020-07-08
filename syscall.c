b59afaa6bee52946660f1de08718ec3fb30b8015
shoeibi98 <46816381+shoeibi98@users.noreply.github.com>
2020-07-08 09:42:47 -0700

    add definition and extern of syscall set_priority

--- syscall.c
+++ syscall.c
@@ -105,6 +105,8 @@ extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_getprocess(void);
 extern int sys_waitx(void);
+extern int sys_set_priority(void);
+
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -129,6 +131,7 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_waitx]   sys_waitx,
+[SYS_set_priority]  sys_set_priority,
 };
 
 void
